9398531b4c8516126c4a1a9d12474ff96188210b
viifr <khanjamil1689@gmail.com>
2026-08-13 15:49:26 +0100

    Add files via upload

---
+++ ExtensionTemplateProject/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("net.portswigger.burp.extensions:montoya-api:2026.7")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
+    options.encoding = "UTF-8"
+}
+
+tasks.named<Jar>("jar") {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(configurations.runtimeClasspath.get().filter { it.isDirectory })
+    from(configurations.runtimeClasspath.get().filterNot { it.isDirectory }.map { zipTree(it) })
+}
